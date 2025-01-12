e7c2285d1a0fc2d332d157b2a1739f8e39047944
Zak Pruitt <pruittzn@gmail.com>
2025-01-12 02:18:40 -0600

    init project

---
+++ src/main/java/com/zakpruitt/pbst/PbstApplication.java
@@ -0,0 +1,13 @@
+package com.zakpruitt.pbst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PbstApplication.class, args);
+	}
+
+}

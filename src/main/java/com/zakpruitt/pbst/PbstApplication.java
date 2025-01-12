ec799b163eb9f96f73ee77cbbc80d0df10a4a899
Zak Pruitt <pruittzn@gmail.com>
2025-01-12 02:28:11 -0600

    init settings

--- src/main/java/com/zakpruitt/pbst/PbstApplication.java
+++ src/main/java/com/zakpruitt/pbst/PbstApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PbstApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
 		SpringApplication.run(PbstApplication.class, args);
 	}
 

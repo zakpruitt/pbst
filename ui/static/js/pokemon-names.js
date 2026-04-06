cbbf1536887dc514d748510faaf4c615392ecce5
Zak Pruitt <pruittzn@gmail.com>
2026-04-06 11:00:00 -0500

    ui: extract CSS and JS to static files, add dashboard charts

---
+++ ui/static/js/pokemon-names.js
@@ -0,0 +1,14 @@
+// gen7x sprite names from msikma/pokesprite (pokemon-gen7x/regular|shiny/{name}.png)
+const POKEMON_NAMES = [
+  'bulbasaur','ivysaur','venusaur','charmander','charmeleon','charizard',
+  'squirtle','wartortle','blastoise','pikachu','raichu','gengar','alakazam',
+  'machamp','golem','gyarados','lapras','eevee','vaporeon','jolteon','flareon',
+  'dragonite','snorlax','mewtwo','mew','espeon','umbreon','scizor','heracross',
+  'tyranitar','blaziken','gardevoir','absol','salamence','metagross','rayquaza',
+  'lucario','garchomp','weavile','glaceon','leafeon','togekiss','gallade',
+  'greninja','talonflame','sylveon','goodra','mimikyu','decidueye','incineroar',
+  'primarina','lycanroc','kommo-o','solgaleo','lunala','ampharos','feraligatr',
+  'typhlosion','meganium','blissey','ninetales','arcanine','haunter','aerodactyl',
+  'articuno','zapdos','moltres','ditto','dragonair','kadabra','electabuzz',
+  'magmar','porygon','rapidash','clefable','wigglytuff','slowbro',
+];
